1e5dc583953e717ff94e8c93de1242115679e47a
jiaohj <739888186@qq.com>
2021-07-20 20:00:12 +0800

    初始化版本

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("sssssss");
+        System.out.println("sssssss");
+        System.out.println("sssssss");
+        System.out.println("sssssss");
+    }
+}
